498f3b6eadb01f7f393628008d25d8ff0f8e014b
Murad Gazymagomedov <quantferox@gmail.com>
2026-07-28 06:14:50 +0400

    dev version

---
+++ src/main/java/com/quantferox/katie/KatieApplication.java
@@ -0,0 +1,13 @@
+package com.quantferox.katie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quantferox/katie/KatieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantferox.katie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
