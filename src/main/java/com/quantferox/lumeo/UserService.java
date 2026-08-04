5b3aeb56f294beaa89fd73e519538d3cbb188032
Murad Gazymagomedov <quantferox@gmail.com>
2026-08-04 23:35:50 +0400

    controller & service & pom.xml

---
+++ src/main/java/com/quantferox/lumeo/UserService.java
@@ -0,0 +1,10 @@
+package com.quantferox.lumeo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+  public String getBannedUser(){
+    return "Get banned Users";
+  }
+}
